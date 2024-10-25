441ee7df87187be7cf9b7e13cf7d86841ed0fcf7
huydatisme <100422956+huydatisme@users.noreply.github.com>
2024-10-25 16:06:44 +0700

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,10 +1,10 @@
 -- create_database.sql
 
-CREATE DATABASE IF NOT EXISTS flaskapp-database;
+CREATE DATABASE IF NOT EXISTS 'flaskapp-database';
 
 USE flaskapp-database;
 
-CREATE TABLE IF NOT EXISTS students (
+CREATE TABLE IF NOT EXISTS 'students' (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
